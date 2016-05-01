eb2ab92c78b140d008436e86790082e7770adc23
xieguigang <xie.guigang@gmail.com>
2016-05-01 11:46:49 +0800

    needs more tweaks

--- IDE_PlugIns/FastaViewer/FormViwer.vb
+++ IDE_PlugIns/FastaViewer/FormViwer.vb
@@ -1,4 +1,5 @@
-﻿Imports LANS.SystemsBiology.SequenceModel.FASTA
+﻿Imports LANS.SystemsBiology.AnalysisTools.SequenceTools.SequencePatterns
+Imports LANS.SystemsBiology.SequenceModel.FASTA
 Imports Microsoft.VisualBasic.Windows.Forms
 
 Public Class FormViwer
@@ -16,19 +17,44 @@ Public Class FormViwer
         Close()
     End Sub
 
+    Dim file As String
+
     Private Sub OpenToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OpenToolStripMenuItem.Click
-        Using open As New OpenFileDialog With {.Filter = ""}
+        Using open As New OpenFileDialog With {
+            .Title = "Select a fasta sequence file",
+            .Filter = "Fasta sequence file(*.fasta,*.fa,*.fsa,*.fas)|*.fasta;*.fa;*.fsa;*.fas"
+        }
             If open.ShowDialog = DialogResult.OK Then
                 Dim fa As New FastaFile(open.FileName)
                 Dim html As String = HTMLRenderer.VisualNts(fa)
                 Dim tmp As String = App.GetAppSysTempFile(".html")
                 Call html.SaveTo(tmp)
                 Call WebBrowser1.Navigate(tmp)
+
+                file = open.FileName
             End If
         End Using
     End Sub
 
     Private Sub FormViwer_Load(sender As Object, e As EventArgs) Handles Me.Load
 
+    End Sub
+
+    Private Sub AboutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AboutToolStripMenuItem.Click
+        Call Process.Start("https://github.com/SMRUCC/GCModeller.Workbench")
+    End Sub
+
+    Private Sub SequenceLogoToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SequenceLogoToolStripMenuItem.Click
+        Dim fa As New FastaFile(file)
+        Dim l As Integer = fa.First.Length
+        For Each x In fa
+            If x.Length <> l Then
+                MsgBox("The fasta sequence length is not identical!")
+                Return
+            End If
+        Next
+
+        Dim logo As Image = SequenceLogo.DrawingDevice.DrawFrequency(fa)
+        PictureBox1.BackgroundImage = logo
     End Sub
 End Class
