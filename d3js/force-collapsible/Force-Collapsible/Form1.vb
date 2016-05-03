7065636d76e1d9c61f83b137a49c4cea2cedfa73
xieguigang <xie.guigang@gmail.com>
2016-05-04 00:41:22 +0800

    add a json network generator

---
+++ d3js/force-collapsible/Force-Collapsible/Form1.vb
@@ -0,0 +1,5 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Call NetworkGenerator.LoadJson("G:\1.13.RegPrecise_network\Cellular Phenotypes\KEGG_Modules-SimpleModsNET").SaveTo("F:\GCModeller.Workbench\d3js\Force-Directed Graph\test.json")
+    End Sub
+End Class
