f83009efd923bc6dd01bb4909b51c0d60a7939f5
关倩倩 <amethyst.asuka@gcmodeller.org>
2016-05-06 02:56:59 +0800

    code readable improvements

--- d3js/Force-Directed Graph/Force-Collapsible/Module1.vb
+++
@@ -1,7 +0,0 @@
-﻿Module Module1
-
-    Sub Main()
-        Call NetworkGenerator.FromRegulations("G:\4.15\MEME\footprints\xcb-modules.TestFootprints2,2.csv").SaveTo("G:\4.15\MEME\footprints\xcb-modules.TestFootprints2,2.json")
-        Call NetworkGenerator.FromRegulations("G:\4.15\MEME\footprints\xcb-pathways.TestFootprints2,2.csv").SaveTo("G:\4.15\MEME\footprints\xcb-pathways.TestFootprints2,2.json")
-    End Sub
-End Module
