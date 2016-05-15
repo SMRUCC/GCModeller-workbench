855fbc6dcfd736828aa3fd552d8f7cfc5544aa10
关倩倩 <amethyst.asuka@gcmodeller.org>
2016-05-15 21:48:48 +0800

    parser for svg in d3js html result

---
+++ d3js/d3svg/d3svg/D3Parser.vb
@@ -0,0 +1,10 @@
+﻿Public MustInherit Class D3Parser
+
+    Public Function HtmlParser(html As String) As SVG
+
+    End Function
+
+    Protected MustOverride Function __css(html As String) As String
+
+
+End Class
