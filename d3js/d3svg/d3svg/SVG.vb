ba2c602886984671557f6718e808bd33ef09af52
关倩倩 <amethyst.asuka@gcmodeller.org>
2016-05-15 21:39:09 +0800

    svg builder for d3js

---
+++ d3js/d3svg/d3svg/SVG.vb
@@ -0,0 +1,21 @@
+﻿Imports System.Runtime.CompilerServices
+Imports System.Text
+
+Public Class SVG
+
+    Public Property CSS As String
+    Public Property Width As Integer
+    Public Property Height As Integer
+    Public Property SVGContent As String
+
+End Class
+
+Public Module SVGBuilder
+
+    <Extension>
+    Public Function Build(svg As SVG) As String
+        Dim sb As New StringBuilder
+
+        Return sb.ToString
+    End Function
+End Module
