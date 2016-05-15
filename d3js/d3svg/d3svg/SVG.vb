ce00eb0e92629b8ef61cfcf28568136bc219f890
关倩倩 <amethyst.asuka@gcmodeller.org>
2016-05-15 21:46:33 +0800

    save svg document

--- d3js/d3svg/d3svg/SVG.vb
+++ d3js/d3svg/d3svg/SVG.vb
@@ -12,10 +12,27 @@ End Class
 
 Public Module SVGBuilder
 
+    Const XmlHead As String = "<?xml version=""1.0"" encoding=""utf-8"" standalone=""no""?>
+<!DOCTYPE svg PUBLIC ""-//W3C//DTD SVG 1.1//EN"" ""http//www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"">
+"
+    Const CSS As String = "<defs><style type=""text/css""><![CDATA[
+{0}
+]]></style></defs>"
+
     <Extension>
     Public Function Build(svg As SVG) As String
-        Dim sb As New StringBuilder
+        Dim sb As New StringBuilder(XmlHead)
+
+        Call sb.AppendLine($"<svg width=""{svg.Width}px"" height=""{svg.Height}px"" version=""1.1"" xmlns=""http://www.w3.org/2000/svg"" xmlns:xlink=""http://www.w3.org/1999/xlink"">")
+        Call sb.AppendLine(String.Format(SVGBuilder.CSS, svg.CSS))
+        Call sb.AppendLine(svg.SVGContent)
+        Call sb.AppendLine("</svg>")
 
         Return sb.ToString
+    End Function
+
+    <Extension>
+    Public Function SaveSVG(svg As SVG, path As String) As Boolean
+        Return svg.Build.SaveTo(path, Encoding.UTF8)
     End Function
 End Module
