4fbdcde5361ca4a13dcb312361059083c0f19ce7
谢桂纲 <xie.guigang@gcmodeller.org>
2016-05-21 18:29:19 +0800

    add a svg to png function, not sure it works or not

---
+++ d3js/d3svg/d3svg/DrawSVG.vb
@@ -0,0 +1,58 @@
+﻿Imports System.Runtime.InteropServices
+
+Public Class DrawSVG
+
+    ''' <summary>
+    ''' The install directory of GIMP library/software
+    ''' </summary>
+    ''' <returns></returns>
+    Public ReadOnly Property GIMP As String
+
+    Sub New(bin As String)
+        GIMP = bin
+    End Sub
+
+    <DllImport("kernel32.dll", SetLastError:=True)>
+    Public Shared Function SetDllDirectory(DIR As String) As Boolean
+    End Function
+
+    <DllImport("libgobject-2.0-0.dll", EntryPoint:="g_type_init", SetLastError:=True)>
+    Public Shared Sub GraphicsTypeInit()
+    End Sub
+
+    <DllImport("librsvg-2-2.dll", EntryPoint:="rsvg_pixbuf_from_file_at_size", SetLastError:=True)>
+    Public Shared Function ReadsvgPixbufFromFileWithSize(path As String, width As Integer, height As Integer, <Out> ByRef [Error] As IntPtr) As IntPtr
+    End Function
+
+    <DllImport("libgdk_pixbuf-2.0-0.dll", EntryPoint:="gdk_pixbuf_save", CallingConvention:=CallingConvention.Cdecl, CharSet:=CharSet.Ansi)>
+    Public Shared Function SaveGdkPixbuf(pixbuf As IntPtr, filename As String, type As String, <Out> ByRef [Error] As IntPtr, __arglist As Object) As Boolean
+    End Function
+
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <param name="svg">SVG inputs file path</param>
+    ''' <param name="png">PNG output path</param>
+    Public Sub RasterizeSvg(svg As String, png As String)
+        Dim callSuccessful As Boolean = SetDllDirectory(GIMP)
+
+        If Not callSuccessful Then
+            Throw New Exception("Could not set DLL directory")
+        End If
+
+        Call GraphicsTypeInit()
+
+        Dim [error] As IntPtr
+        Dim result As IntPtr = ReadsvgPixbufFromFileWithSize(svg, -1, -1, [error])
+
+        If [error] <> IntPtr.Zero Then
+            Throw New Exception(Marshal.ReadInt32([error]).ToString())
+        End If
+
+        callSuccessful = SaveGdkPixbuf(result, png, "png", [error], Nothing)
+
+        If Not callSuccessful Then
+            Throw New Exception([error].ToInt32().ToString())
+        End If
+    End Sub
+End Class
