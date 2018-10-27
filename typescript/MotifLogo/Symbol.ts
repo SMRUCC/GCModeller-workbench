289945a13f87a9fa81447ba82e5b7d2c7cc4952c
xieguigang <xie.guigang@gcmodeller.org>
2018-10-27 13:25:42 +0800

    symbol model

---
+++ typescript/MotifLogo/Symbol.ts
@@ -0,0 +1,29 @@
+﻿namespace GCModeller.Workbench {
+
+    export class Symbol {
+
+        public symbol;
+        public scale;
+        public colour;
+
+        public constructor(alph_index, scale, alphabet) {
+            this.symbol = alphabet.get_letter(alph_index);
+            this.scale = scale;
+            this.colour = alphabet.get_colour(alph_index);
+        }
+
+        public toString(): string {
+            return this.symbol + " " + (Math.round(this.scale * 1000) / 10) + "%";
+        }
+
+        public static compareSymbol(sym1: Symbol, sym2: Symbol): number {
+            if (sym1.scale < sym2.scale) {
+                return -1;
+            } else if (sym1.scale > sym2.scale) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+    }
+}
