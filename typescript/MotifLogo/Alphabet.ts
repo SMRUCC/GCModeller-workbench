c6b6f761f2df201e4691558ac49e0aa37dbcccc8
xieguigang <xie.guigang@gcmodeller.org>
2018-10-27 12:49:59 +0800

    add scripts

---
+++ typescript/MotifLogo/Alphabet.ts
@@ -0,0 +1,16 @@
+﻿namespace GCModeller.Workbench {
+
+    export class Alphabet {
+        public is_letter;
+        public is_prob;
+        public pos;
+        public letter;
+        public parts;
+        public i;
+        public freq;
+
+        public constructor() {
+
+        }
+    }
+}
