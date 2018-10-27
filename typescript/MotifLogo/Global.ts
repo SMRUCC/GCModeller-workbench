0bbaaa54b7182d9269953555c753178cf482cdb9
xieguigang <xie.guigang@gcmodeller.org>
2018-10-27 14:38:09 +0800

    code style improvements

--- typescript/MotifLogo/Global.ts
+++ typescript/MotifLogo/Global.ts
@@ -1,13 +1,13 @@
 ﻿namespace GCModeller.Workbench {
 
-    /*
- * Fast string trimming implementation found at
- * http://blog.stevenlevithan.com/archives/faster-trim-javascript
- *
- * Note that regex is good at removing leading space but
- * bad at removing trailing space as it has to first go through
- * the whole string.
- */
+    /**
+     * Fast string trimming implementation found at
+     * http://blog.stevenlevithan.com/archives/faster-trim-javascript
+     *
+     * Note that regex is good at removing leading space but
+     * bad at removing trailing space as it has to first go through
+     * the whole string.
+    */
     export function trim(str: string): string {
         "use strict";
 
@@ -15,6 +15,7 @@
         str = str.replace(/^\s\s*/, '');
         ws = /\s/; i = str.length;
         while (ws.test(str.charAt(--i)));
+
         return str.slice(0, i + 1);
     }
 }
