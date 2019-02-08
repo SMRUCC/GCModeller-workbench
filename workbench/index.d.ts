8ec43204f70d490fe5f7df228541ea9517997a27
xieguigang <xie.guigang@gcmodeller.org>
2019-02-08 12:33:33 +0800

    electron is not working with typescript

---
+++ workbench/index.d.ts
@@ -0,0 +1,5 @@
+declare const app: Electron.App, BrowserWindow: typeof Electron.BrowserWindow, Menu: typeof Electron.Menu, Notification: typeof Electron.Notification;
+declare var template: any;
+declare let win: Electron.BrowserWindow;
+declare function renderAppMenu(): void;
+declare function createWindow(): void;
--- workbench/main.ts
+++ workbench/main.ts
@@ -1,58 +1,74 @@
 /// <reference path="node_modules/electron/electron.d.ts" />
+
 // load framework
 const { app, BrowserWindow, Menu, Notification } = require('electron');
+
 // load internal app components
 var template = require("./menu.json");
+
 // 保持对window对象的全局引用，如果不这么做的话，当JavaScript对象被
 // 垃圾回收的时候，window对象将会自动的关闭
-let win = null;
+let win: Electron.BrowserWindow = null;
+
 function renderAppMenu() {
+
     // replace all url as menu click
     template.forEach(m => m.submenu.filter(sm => "click" in sm).forEach(sm => {
         var url = sm.click + "";
         sm.click = function () { require('electron').shell.openExternal(url); };
     }));
+
     console.log(template);
-    const menu = Menu.buildFromTemplate(template);
-    Menu.setApplicationMenu(menu);
+
+    const menu = Menu.buildFromTemplate(template)
+    Menu.setApplicationMenu(menu)
 }
+
 function createWindow() {
     // 创建浏览器窗口。
-    win = new BrowserWindow({ width: 800, height: 600 });
+    win = new BrowserWindow({ width: 800, height: 600 })
+
     // 然后加载应用的 index.html。
-    win.loadFile('./views/index.html');
+    win.loadFile('./views/index.html')
+
     // 打开开发者工具
-    win.webContents.openDevTools();
+    win.webContents.openDevTools()
+
     // 当 window 被关闭，这个事件会被触发。
     win.on('closed', () => {
         // 取消引用 window 对象，如果你的应用支持多窗口的话，
         // 通常会把多个 window 对象存放在一个数组里面，
         // 与此同时，你应该删除相应的元素。
-        win = null;
+        win = null
     });
+
     renderAppMenu();
+
     var msg = new Electron.Notification({ title: "Task Finish", body: "test task finished!" });
     msg.show();
 }
+
 // Electron 会在初始化后并准备
 // 创建浏览器窗口时，调用这个函数。
 // 部分 API 在 ready 事件触发后才能使用。
-app.on('ready', createWindow);
+app.on('ready', createWindow)
+
 // 当全部窗口关闭时退出。
 app.on('window-all-closed', () => {
     // 在 macOS 上，除非用户用 Cmd + Q 确定地退出，
     // 否则绝大部分应用及其菜单栏会保持激活。
     if (process.platform !== 'darwin') {
-        app.quit();
+        app.quit()
     }
-});
+})
+
 app.on('activate', () => {
     // 在macOS上，当单击dock图标并且没有其他窗口打开时，
     // 通常在应用程序中重新创建一个窗口。
     if (win === null) {
-        createWindow();
+        createWindow()
     }
-});
+})
+
 // 在这个文件中，你可以续写应用剩下主进程代码。
 // 也可以拆分成几个文件，然后用 require 导入。
-//# sourceMappingURL=index.js.map
