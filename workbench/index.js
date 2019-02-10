d1faaa3bc6eb89c31ced56ed27f3fc4cc3a8f134
xieguigang <xie.guigang@gcmodeller.org>
2019-02-10 16:59:55 +0800

    open windows test success

--- workbench/index.js
+++ workbench/index.js
@@ -27,39 +27,61 @@ var helpers;
         return menu;
     }
     helpers.renderAppMenu = renderAppMenu;
+    // 保持对window对象的全局引用，如果不这么做的话，当JavaScript对象被
+    // 垃圾回收的时候，window对象将会自动的关闭
+    var windows = {};
+    function getMainWindow() {
+        if (mainView in windows) {
+            return windows[mainView];
+        }
+        else {
+            return null;
+        }
+    }
+    helpers.getMainWindow = getMainWindow;
+    function createWindow(view, size = [800, 600], callback = null, lambda = false, debug = false) {
+        var invoke = function () {
+            // 创建浏览器窗口。
+            let win = new BrowserWindow({ width: size[0], height: size[1] });
+            // 然后加载应用的 index.html。
+            win.loadFile(view);
+            if (debug) {
+                // 打开开发者工具
+                win.webContents.openDevTools();
+            }
+            // 当 window 被关闭，这个事件会被触发。
+            win.on('closed', () => {
+                // 取消引用 window 对象，如果你的应用支持多窗口的话，
+                // 通常会把多个 window 对象存放在一个数组里面，
+                // 与此同时，你应该删除相应的元素。
+                windows[view] = null;
+            });
+            if (callback) {
+                callback();
+            }
+            return function () {
+            };
+        };
+        return lambda ? invoke : invoke();
+    }
+    helpers.createWindow = createWindow;
 })(helpers || (helpers = {}));
 /// <reference path="node_modules/electron/electron.d.ts" />
 /// <reference path="dev/helper.ts" />
 // load framework
 const { app, BrowserWindow, Menu, Notification } = require('electron');
+const mainView = "./views/index.html";
 // load internal app components
 var template = require("./menu.json");
-// 保持对window对象的全局引用，如果不这么做的话，当JavaScript对象被
-// 垃圾回收的时候，window对象将会自动的关闭
-let win = null;
 let menu = null;
-function createWindow() {
-    // 创建浏览器窗口。
-    win = new BrowserWindow({ width: 800, height: 600 });
-    // 然后加载应用的 index.html。
-    win.loadFile('./views/index.html');
-    // 打开开发者工具
-    win.webContents.openDevTools();
-    // 当 window 被关闭，这个事件会被触发。
-    win.on('closed', () => {
-        // 取消引用 window 对象，如果你的应用支持多窗口的话，
-        // 通常会把多个 window 对象存放在一个数组里面，
-        // 与此同时，你应该删除相应的元素。
-        win = null;
-    });
-    menu = helpers.renderAppMenu(template);
-    var msg = new Notification({ title: "Task Finish", body: "test task finished!" });
-    msg.show();
-}
 // Electron 会在初始化后并准备
 // 创建浏览器窗口时，调用这个函数。
 // 部分 API 在 ready 事件触发后才能使用。
-app.on('ready', createWindow);
+app.on('ready', helpers.createWindow(mainView, [800, 600], function () {
+    menu = helpers.renderAppMenu(template);
+    var msg = new Notification({ title: "Task Finish", body: "test task finished!" });
+    msg.show();
+}, true));
 // 当全部窗口关闭时退出。
 app.on('window-all-closed', () => {
     // 在 macOS 上，除非用户用 Cmd + Q 确定地退出，
@@ -71,8 +93,8 @@ app.on('window-all-closed', () => {
 app.on('activate', () => {
     // 在macOS上，当单击dock图标并且没有其他窗口打开时，
     // 通常在应用程序中重新创建一个窗口。
-    if (win === null) {
-        createWindow();
+    if (helpers.getMainWindow() === null) {
+        helpers.createWindow(mainView);
     }
 });
 // 在这个文件中，你可以续写应用剩下主进程代码。
---
+++ workbench/views/common.js
@@ -0,0 +1,7 @@
+function openView(view) {
+    const remote = require('electron').remote;
+    const BrowserWindow = remote.BrowserWindow;
+
+    var win = new BrowserWindow({ width: 800, height: 600 });
+    win.loadFile(view);
+}
