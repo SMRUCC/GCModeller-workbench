46de36a1caf1fc650bbcf3b23b34efcd9c322bce
xieguigang <xie.guigang@gcmodeller.org>
2019-07-13 02:29:24 +0800

    update packages

---
+++ typescript/build/InteractionViz.d.ts
@@ -0,0 +1,7 @@
+declare namespace GCModeller.Workbench {
+    /**
+     * 基于``sigma.js``的蛋白互做网络可视化组件
+    */
+    class Viewer {
+    }
+}
---
+++ typescript/build/InteractionViz.js
@@ -0,0 +1,16 @@
+var GCModeller;
+(function (GCModeller) {
+    var Workbench;
+    (function (Workbench) {
+        /**
+         * 基于``sigma.js``的蛋白互做网络可视化组件
+        */
+        var Viewer = /** @class */ (function () {
+            function Viewer() {
+            }
+            return Viewer;
+        }());
+        Workbench.Viewer = Viewer;
+    })(Workbench = GCModeller.Workbench || (GCModeller.Workbench = {}));
+})(GCModeller || (GCModeller = {}));
+//# sourceMappingURL=InteractionViz.js.map
