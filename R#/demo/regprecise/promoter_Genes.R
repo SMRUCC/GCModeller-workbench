d69fa03a68fed074e3ada9f6bd6df641cda3aeac
xieguigang <xie.guigang@gcmodeller.org>
2020-05-20 05:07:12 +0800

    bind data

--- R#/demo/regprecise/promoter_Genes.R
+++ R#/demo/regprecise/promoter_Genes.R
@@ -57,10 +57,39 @@ data.frame(regulator = regulator, n = n, gene_list = gene_list)
 
 # str(regs);
 
-let expr = read.dataframe("K:\20200226\20200516_gbk\Archive 2\转录组蛋白质组结果1_细胞.csv", mode = "character");
+let bindData as function(path) {
+let expr = read.dataframe(path, mode = "character");
+let cols = dataset.colnames(expr); 
+
+print(cols);
 
 # print(head(expr));
 
-expr = expr :> groupBy(a => as.object(a)$ID) :> lapply(a => a[1], names = a => a$key);
+expr = expr :> groupBy(a => as.object(a)$ID) :> lapply(a => as.object(a[1]), names = a => a$key);
 
 print(names(expr));
+
+let reg_id = sapply(regs, a => a$gene);
+
+let out = data.frame(gene_id = reg_id, regulator = sapply(regs, a => a$regulator));
+let col_vals = NULL;
+
+for (name in cols) {
+	col_vals = sapply(reg_id, function(a) {
+		let x = expr[[a]];
+		x[[name]];
+	});
+	
+	print(col_vals);
+	out = rbind(out, col_vals);
+}
+
+colnames(out) = (["gene_id", "regulator"] << cols);
+
+write.csv(out, file = `${dirname(path)}/regulators_${basename(path)}.csv`, row_names = FALSE);
+
+}
+
+
+bindData("K:\20200226\20200516_gbk\Archive 2\转录组蛋白质组结果1_细胞.csv");
+bindData("K:\20200226\20200516_gbk\Archive 2\转录组蛋白质组结果2_上清.csv");
