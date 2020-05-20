0855d13acc7c212ad769a8497ede4dda74b86c20
xieguigang <xie.guigang@gcmodeller.org>
2020-05-20 22:21:13 +0800

    common js project

---
+++ typescript/common/app.ts
@@ -0,0 +1,23 @@
+function openView(view) {
+	const remote = require('electron').remote;
+	const BrowserWindow = remote.BrowserWindow;
+
+	let win = new BrowserWindow({ width: 800, height: 600 });
+	win.loadFile(view);
+	return win;
+	// win.webContents.openDevTools();
+}
+
+function ipc_sendData(key, value, win) {
+	// main process
+	win.webContents.send(key, value);
+}
+
+function getData(key, action) {
+	// renderer process
+	let ipcRenderer = require('electron').ipcRenderer;
+
+	ipcRenderer.on(key, function (event, data) {
+		action(store);
+	});
+}
