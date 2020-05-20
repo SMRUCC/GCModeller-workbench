fc5988bc9acc57e45825960a3cbf9e5cf094f404
xieguigang <xie.guigang@gcmodeller.org>
2020-05-20 22:24:08 +0800

    compile script

--- typescript/common/app.ts
+++ typescript/common/app.ts
@@ -1,23 +1,23 @@
-function openView(view) {
-	const remote = require('electron').remote;
-	const BrowserWindow = remote.BrowserWindow;
+function openView(view: string) {
+    const remote = require('electron').remote;
+    const BrowserWindow = remote.BrowserWindow;
 
-	let win = new BrowserWindow({ width: 800, height: 600 });
-	win.loadFile(view);
-	return win;
-	// win.webContents.openDevTools();
+    let win = new BrowserWindow({ width: 800, height: 600 });
+    win.loadFile(view);
+    // win.webContents.openDevTools();
+    return win;
 }
 
-function ipc_sendData(key, value, win) {
-	// main process
-	win.webContents.send(key, value);
+function ipc_sendData(key: string, value: string, win) {
+    // main process
+    win.webContents.send(key, value);
 }
 
-function getData(key, action) {
-	// renderer process
-	let ipcRenderer = require('electron').ipcRenderer;
+function getData(key: string, action: (data: string) => void) {
+    // renderer process
+    let ipcRenderer = require('electron').ipcRenderer;
 
-	ipcRenderer.on(key, function (event, data) {
-		action(store);
-	});
+    ipcRenderer.on(key, function (event, data) {
+        action(data);
+    });
 }
