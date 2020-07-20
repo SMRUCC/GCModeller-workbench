369df86555baeff7c7b8cc3d4b3646710e235fad
xieguigang <xie.guigang@gcmodeller.org>
2020-07-21 02:09:04 +0800

    Update all_bacterials.R

--- R#/demo/cngb/all_bacterials.R
+++ R#/demo/cngb/all_bacterials.R
@@ -4,15 +4,38 @@ imports "ftp" from "R.web";
 setwd(!script$dir);
 
 let prokaryote = fetch.kegg_organism(NULL, type = "prokaryote");
-let cngb as new ftp(server = "ftp.cngb.org");
+let cngb = new ftp(server = "ftp.cngb.org");
+
+print(cngb);
 
 write.csv(prokaryote, file = "./bacterials.csv", row_names = FALSE);
 
-for(genome in prokaryote :> projectAs(as.object)) {
+let downloadGbff as function(genome) {
 	let assembly as string = strsplit(gsub(genome$RefSeq, "ftp://ftp.ncbi.nlm.nih.gov/genomes/all/", ""), "/");
-	print(assembly);
 	let assemblyName = `${assembly[1]}_${paste(assembly[2:4], "")}`;
-	let ftpURL = `ftp://ftp.cngb.org/pub/Assembly/${paste(assembly, "/")}/`;
+	let ftpURL = `/pub/Assembly/${paste(assembly[1:4], "/")}`;
+	
+	print(`[${assemblyName}] => ${ftpURL}`);
+	
+	let dirs = cngb :> list.ftp_dirs(ftpURL);
+	
+	print(dirs);
+	
+	for(name in dirs) {
+		let subdir = `${ftpURL}/${name}`;
+		let files = cngb :> list.ftp_dirs(subdir);
+		let gbff = files :> which(name => name == $".+genomic.gbff.gz");
+		
+		# print(files);
+		print(gbff);
+		
+	}
+}
+
+downloadGbff(as.object(prokaryote[1]));
+
+for(genome in prokaryote :> projectAs(as.object)) {
+	
 	
 	break;
 }
