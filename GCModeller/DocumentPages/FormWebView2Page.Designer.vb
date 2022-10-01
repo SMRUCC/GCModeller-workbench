e8ea6ffc0bce1c64ff87d15407b51ff60f9b4c82
xieguigang <xie.guigang@gcmodeller.org>
2022-10-01 15:04:17 +0800

    how to add webview2?

--- GCModeller/DocumentPages/FormWebView2Page.Designer.vb
+++ GCModeller/DocumentPages/FormWebView2Page.Designer.vb
@@ -22,9 +22,16 @@ Partial Class FormWebView2Page
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.SuspendLayout()
+        '
+        'FormWebView2Page
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(1042, 643)
+        Me.Name = "FormWebView2Page"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
     End Sub
 End Class
