469d96375845cd52ea044a58942dada76adc9dcf
xieguigang <xie.guigang@gcmodeller.org>
2022-10-01 14:45:58 +0800

    update web view from chrome to webview2

--- GCModeller/FormMain.vb
+++ GCModeller/FormMain.vb
@@ -1,26 +1,4 @@
-﻿Imports System.ComponentModel
-Imports CefSharp
-Imports CefSharp.WinForms
+﻿Public Class FormMain
 
-Public Class FormMain
 
-    Dim WithEvents browser As ChromiumWebBrowser
-
-    Private Sub FormMain_Load(sender As Object, e As EventArgs) Handles Me.Load
-        Dim settings As New CefSettings()
-        Dim browserSettings As New BrowserSettings()
-
-        Call Cef.Initialize(settings)
-
-        browser = New ChromiumWebBrowser("https://gcmodeller.org/") With {
-           .Dock = DockStyle.Fill,
-           .BrowserSettings = browserSettings
-        }
-
-        Controls.Add(browser)
-    End Sub
-
-    Private Sub FormMain_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
-        Call Cef.Shutdown()
-    End Sub
 End Class
