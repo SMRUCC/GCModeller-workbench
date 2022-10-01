9132aa47c3ef2d4d94d8e2cdf172a7efea5a1782
xieguigang <xie.guigang@gcmodeller.org>
2022-10-01 14:51:34 +0800

    config for run app

--- GCModeller/My Project/Application.Designer.vb
+++ GCModeller/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.GCModeller.FormMain
+            Me.MainForm = Global.GCModeller.FormMainHost
         End Sub
     End Class
 End Namespace
