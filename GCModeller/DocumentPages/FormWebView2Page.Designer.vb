d34134920c552def6825d76362b38ad1c05ed23f
xieguigang <xie.guigang@gcmodeller.org>
2022-10-01 15:13:12 +0800

    add page url

--- GCModeller/DocumentPages/FormWebView2Page.Designer.vb
+++ GCModeller/DocumentPages/FormWebView2Page.Designer.vb
@@ -22,16 +22,34 @@ Partial Class FormWebView2Page
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.WebView21 = New Microsoft.Web.WebView2.WinForms.WebView2()
+        CType(Me.WebView21, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'WebView21
+        '
+        Me.WebView21.AllowExternalDrop = True
+        Me.WebView21.CreationProperties = Nothing
+        Me.WebView21.DefaultBackgroundColor = System.Drawing.Color.White
+        Me.WebView21.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.WebView21.Location = New System.Drawing.Point(0, 0)
+        Me.WebView21.Name = "WebView21"
+        Me.WebView21.Size = New System.Drawing.Size(1042, 643)
+        Me.WebView21.TabIndex = 0
+        Me.WebView21.ZoomFactor = 1.0R
         '
         'FormWebView2Page
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1042, 643)
+        Me.Controls.Add(Me.WebView21)
         Me.Name = "FormWebView2Page"
         Me.Text = "Form1"
+        CType(Me.WebView21, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
+
+    Friend WithEvents WebView21 As Microsoft.Web.WebView2.WinForms.WebView2
 End Class
--- GCModeller/DocumentPages/FormWebView2Page.vb
+++ GCModeller/DocumentPages/FormWebView2Page.vb
@@ -1,3 +1,6 @@
 ﻿Public Class FormWebView2Page
 
+    Private Sub FormWebView2Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+        WebView21.CoreWebView2.Navigate("https://gcmodeller.org/")
+    End Sub
 End Class
