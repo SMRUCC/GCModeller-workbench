3bec93781103ca885744e7d7a06b01cafb8f0ecd
xieguigang <xie.guigang@gcmodeller.org>
2022-10-01 18:11:20 +0800

    how to start a new web services process

--- src/GCModeller/Application/Globals.vb
+++ src/GCModeller/Application/Globals.vb
@@ -1,5 +1,23 @@
-﻿Public Class Globals
+﻿Imports Microsoft.VisualBasic.CommandLine.InteropService.Pipeline
+Imports Microsoft.VisualBasic.Parallel
+
+Public Class Globals
 
     Friend Shared host As FormMainHost
+
+    Shared wwwroot As RunSlavePipeline
+
+    Public Shared Sub Load()
+        Call startWebServices()
+    End Sub
+
+    Private Shared Sub startWebServices()
+        Dim host = Rserver.RscriptCommandLine.Rscript.FromEnvironment($"{App.HOME}/")
+        Dim commandLine As String = $""
+
+        wwwroot = host.CreateSlave(commandLine)
+
+        Call RunTask(AddressOf wwwroot.Run)
+    End Sub
 
 End Class
