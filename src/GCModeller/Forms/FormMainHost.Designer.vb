4ff141a1892bd30372ceda27563fc74b556c7990
xieguigang <xie.guigang@gcmodeller.org>
2022-10-01 15:38:37 +0800

    config UI

--- src/GCModeller/Forms/FormMainHost.Designer.vb
+++ src/GCModeller/Forms/FormMainHost.Designer.vb
@@ -1,4 +1,6 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿Imports WeifenLuo.WinFormsUI.Docking
+
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class FormMainHost
     Inherits System.Windows.Forms.Form
 
@@ -22,10 +24,82 @@ Partial Class FormMainHost
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.components = New System.ComponentModel.Container()
+        Me.dockPanel = New WeifenLuo.WinFormsUI.Docking.DockPanel()
+        Me.vS2015DarkTheme1 = New WeifenLuo.WinFormsUI.Docking.VS2015DarkTheme()
+        Me.vS2015BlueTheme1 = New WeifenLuo.WinFormsUI.Docking.VS2015BlueTheme()
+        Me.vS2015LightTheme1 = New WeifenLuo.WinFormsUI.Docking.VS2015LightTheme()
+        Me.vsToolStripExtender1 = New WeifenLuo.WinFormsUI.Docking.VisualStudioToolStripExtender(Me.components)
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.ToolStripProgressBar1 = New System.Windows.Forms.ToolStripProgressBar()
+        Me.StatusStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'dockPanel
+        '
+        Me.dockPanel.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.dockPanel.DockBackColor = System.Drawing.Color.FromArgb(CType(CType(41, Byte), Integer), CType(CType(57, Byte), Integer), CType(CType(85, Byte), Integer))
+        Me.dockPanel.DockBottomPortion = 150.0R
+        Me.dockPanel.DockLeftPortion = 200.0R
+        Me.dockPanel.DockRightPortion = 200.0R
+        Me.dockPanel.DockTopPortion = 150.0R
+        Me.dockPanel.Font = New System.Drawing.Font("Tahoma", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.World, CType(0, Byte))
+        Me.dockPanel.Location = New System.Drawing.Point(0, 0)
+        Me.dockPanel.Name = "dockPanel"
+        Me.dockPanel.RightToLeftLayout = True
+        Me.dockPanel.ShowAutoHideContentOnHover = False
+        Me.dockPanel.Size = New System.Drawing.Size(800, 428)
+        Me.dockPanel.TabIndex = 0
+        '
+        'vsToolStripExtender1
+        '
+        Me.vsToolStripExtender1.DefaultRenderer = Nothing
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel1, Me.ToolStripProgressBar1})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 428)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(800, 22)
+        Me.StatusStrip1.TabIndex = 2
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'ToolStripStatusLabel1
+        '
+        Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
+        Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(119, 17)
+        Me.ToolStripStatusLabel1.Text = "ToolStripStatusLabel1"
+        '
+        'ToolStripProgressBar1
+        '
+        Me.ToolStripProgressBar1.Name = "ToolStripProgressBar1"
+        Me.ToolStripProgressBar1.Size = New System.Drawing.Size(100, 16)
+        '
+        'FormMainHost
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.dockPanel)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.IsMdiContainer = True
+        Me.Name = "FormMainHost"
         Me.Text = "Form1"
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Private WithEvents dockPanel As WeifenLuo.WinFormsUI.Docking.DockPanel
+    Private WithEvents vsToolStripExtender1 As WeifenLuo.WinFormsUI.Docking.VisualStudioToolStripExtender
+
+    Private vS2015LightTheme1 As WeifenLuo.WinFormsUI.Docking.VS2015LightTheme
+    Private vS2015BlueTheme1 As WeifenLuo.WinFormsUI.Docking.VS2015BlueTheme
+    Private vS2015DarkTheme1 As WeifenLuo.WinFormsUI.Docking.VS2015DarkTheme
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents ToolStripStatusLabel1 As ToolStripStatusLabel
+    Friend WithEvents ToolStripProgressBar1 As ToolStripProgressBar
 End Class
--- src/GCModeller/Forms/FormMainHost.vb
+++ src/GCModeller/Forms/FormMainHost.vb
@@ -1,3 +1,57 @@
-﻿Public Class FormMainHost
+﻿Imports WeifenLuo.WinFormsUI.Docking
 
+Public Class FormMainHost
+
+    Private ReadOnly _toolStripProfessionalRenderer As New ToolStripProfessionalRenderer()
+
+    Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        AutoScaleMode = AutoScaleMode.Dpi
+        vsToolStripExtender1.DefaultRenderer = _toolStripProfessionalRenderer
+    End Sub
+
+    Private Sub FormMainHost_Load(sender As Object, e As EventArgs) Handles Me.Load
+        dockPanel.Theme = vS2015LightTheme1
+        Me.EnableVSRenderer(VisualStudioToolStripExtender.VsVersion.Vs2015, vS2015LightTheme1)
+    End Sub
+
+    Private Function FindDocument(ByVal text As String) As IDockContent
+        If dockPanel.DocumentStyle = DocumentStyle.SystemMdi Then
+            For Each form In MdiChildren
+                If Equals(form.Text, text) Then Return TryCast(form, IDockContent)
+            Next
+
+            Return Nothing
+        Else
+
+            For Each content In dockPanel.Documents
+                If Equals(content.DockHandler.TabText, text) Then Return content
+            Next
+
+            Return Nothing
+        End If
+    End Function
+
+    Private Sub CloseAllDocuments()
+        If dockPanel.DocumentStyle = DocumentStyle.SystemMdi Then
+            For Each form In MdiChildren
+                form.Close()
+            Next
+        Else
+
+            For Each document In dockPanel.DocumentsToArray()
+                ' IMPORANT: dispose all panes.
+                document.DockHandler.DockPanel = Nothing
+                document.DockHandler.Close()
+            Next
+        End If
+    End Sub
+
+    Private Sub EnableVSRenderer(version As VisualStudioToolStripExtender.VsVersion, theme As ThemeBase)
+        vsToolStripExtender1.SetStyle(StatusStrip1, version, theme)
+    End Sub
 End Class
