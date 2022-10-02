2ed4b15ba915585d13d89acfd7be0177d81cb03d
xieguigang <xie.guigang@gcmodeller.org>
2022-10-02 17:10:28 +0800

    Update imports_uniprot.R

--- fastRWeb/database/imports_uniprot.R
+++ fastRWeb/database/imports_uniprot.R
@@ -1,6 +1,36 @@
 require(JSON);
 require(GCModeller);
 
+imports "ptf" from "annotationKit";
+imports "uniprot" from "seqtoolkit";
+
 const run = function(file) {
+    const savedb as string = `/etc/repository/ptf/${md5(basename(file))}.db`;
+    const result as string = 
     
+    try({
+        file 
+        |> open.uniprot(file)
+        |> cache.ptf(file = savedb, hds.stream = TRUE)
+        ;
+
+        "success";
+    })
+    ;
+
+    if (is.null(result)) {
+        json_encode({
+            code: 500, 
+            info: as.list(traceback())
+        }) 
+        |> writeLines(con = buffer("text"))
+        ;
+    } else {
+        json_encode({
+            code: 0, 
+            info: savedb
+        }) 
+        |> writeLines(con = buffer("text"))
+        ;
+    }
 }
