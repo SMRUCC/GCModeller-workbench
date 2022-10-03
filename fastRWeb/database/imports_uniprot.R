6f878b5fff00856638381e48037c5ba2a5bdd6c2
xieguigang <xie.guigang@gcmodeller.org>
2022-10-04 01:00:06 +0800

    do parameter validation

--- fastRWeb/database/imports_uniprot.R
+++ fastRWeb/database/imports_uniprot.R
@@ -7,38 +7,50 @@ imports "uniprot" from "seqtoolkit";
 const run = function(file, name, note = "") {
     const savedb as string = `/etc/repository/ptf/${md5(name)}.db`;
     const metafile as string = `/etc/repository/ptf/${md5(name)}.json`;
-    const result as string = 
 
-    try({
-        file 
-        |> open.uniprot(file)
-        |> cache.ptf(file = savedb, hds.stream = TRUE)
-        ;
-
-        json_encode({
-            name: name,
-            note: note
-        })
-        |> writeLines(con = metafile)
-        ;
-
-        "success";
-    })
-    ;
-
-    if (is.null(result)) {
+    if (nchar(name) == 0) {
         json_encode({
             code: 500, 
-            info: as.list(traceback())
+            info: "The database name can not be empty!"
         }) 
         |> writeLines(con = buffer("text"))
         ;
     } else {
-        json_encode({
-            code: 0, 
-            info: savedb
-        }) 
-        |> writeLines(con = buffer("text"))
+        const result as string = try({
+            run_imports(file, savedb, name, note, metafile)
+        })
         ;
-    }
+
+        if (is.null(result)) {
+            json_encode({
+                code: 500, 
+                info: as.list(traceback())
+            }) 
+            |> writeLines(con = buffer("text"))
+            ;
+        } else {
+            json_encode({
+                code: 0, 
+                info: savedb
+            }) 
+            |> writeLines(con = buffer("text"))
+            ;
+        }
+    }    
+}
+
+const run_imports = function(file, savedb, name, note, metafile) {
+    file 
+    |> open.uniprot()
+    |> cache.ptf(file = savedb, hds.stream = TRUE)
+    ;
+
+    json_encode({
+        name: name,
+        note: note
+    })
+    |> writeLines(con = metafile)
+    ;
+
+    "success";
 }
