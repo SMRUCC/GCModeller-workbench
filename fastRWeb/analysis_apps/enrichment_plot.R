3eeffe73106d6e60639b0e73fda9900d39b5e424
xieguigang <xie.guigang@gcmodeller.org>
2022-10-04 17:48:26 +0800

    add uniprot keywords data

--- fastRWeb/analysis_apps/enrichment_plot.R
+++ fastRWeb/analysis_apps/enrichment_plot.R
@@ -2,19 +2,46 @@ require(JSON);
 require(GCModeller);
 
 imports "visualPlot" from "visualkit";
-imports "GSEA" from "gseakit";
+imports ["GSEA", "UniProt"] from "gseakit";
 
-const run = function(session_id, type = ["bar", "bubble"]) {
+const run = function(session_id, type = ["bar", "bubble"], background = "", top = 5) {
     const session_file as string = `${getOption("system_tempdir")}/${session_id}/enrichment.dat`;
     const enrichment = readRDS(session_file);
+    const terms = GSEA::to_enrichment_terms(enrichment);
 
     type = type[1] || "bar";
 
     print("previews of the enrichment result:");
     print(enrichment, max.print = 6);
+    # print(head(terms));
     print("start to run data plots!");
     print("The plot data type:");
     print(type);
 
+    let enrich_profiles = NULL;
 
+    if ([!is.null(background)] && [background == "keyword"]) {
+        let keywords = "E:\GCModeller\src\workbench\win32_desktop\src\Rstudio\data\UniProt-Keywords.tsv"
+        |> read.csv(tsv = TRUE, check.names = FALSE)
+        ;
+
+        enrich_profiles = UniProt::keyword_profiles(terms, keywords, top = top);
+    } else {
+        stop("not implemented");
+    }
+
+    if (type == "bar") {
+        bitmap(file = buffer("bitmap")) {
+            category_profiles.plot(
+                profiles ,
+                colors = "paper",
+                title = "UniProt Keywords",
+                axis.title = "-log10(p-value)",
+                size = [1200,1600],
+                dpi = 100
+            );
+        }
+    } else {
+        stop("not implemented");
+    }
 }
