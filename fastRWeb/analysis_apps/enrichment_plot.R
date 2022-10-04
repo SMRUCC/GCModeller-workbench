cdcbcfde59a4cbb8787e99b50b52e76e6385bac9
xieguigang <xie.guigang@gcmodeller.org>
2022-10-04 17:30:08 +0800

    cast data terms

--- fastRWeb/analysis_apps/enrichment_plot.R
+++ fastRWeb/analysis_apps/enrichment_plot.R
@@ -1,10 +1,20 @@
 require(JSON);
 require(GCModeller);
 
-const run = function(session_id) {
+imports "visualPlot" from "visualkit";
+imports "GSEA" from "gseakit";
+
+const run = function(session_id, type = ["bar", "bubble"]) {
     const session_file as string = `${getOption("system_tempdir")}/${session_id}/enrichment.dat`;
     const enrichment = readRDS(session_file);
 
+    type = type[1] || "bar";
+
+    print("previews of the enrichment result:");
     print(enrichment, max.print = 6);
-    
+    print("start to run data plots!");
+    print("The plot data type:");
+    print(type);
+
+
 }
