32f454432146428bc13b34860c3d0169826984db
xieguigang <xie.guigang@gcmodeller.org>
2022-10-05 14:35:56 +0800

    Update inspect_database.R

--- fastRWeb/database/inspect_database.R
+++ fastRWeb/database/inspect_database.R
@@ -1,10 +1,15 @@
 require(HDS);
 require(JSON);
+require(GCModeller);
+
+imports "ptf" from "annotationKit";
 
 const run = function(guid) {
     const db = HDS::openStream(`/etc/repository/ptf/${guid}.db`);
     const protein_counts = HDS::getText(db, "/metadata/count.txt");
-    const protein_id = HDS::getText(db, "/metadata/proteins.txt");
+    const xrefs = ptf::list.xrefs(db);
+    const summary_info = ptf::summary.xrefs(db, xrefs);
+    # const protein_id = HDS::getText(db, "/metadata/proteins.txt");
 
     # print(HDS::tree(db));
 
@@ -12,7 +17,8 @@ const run = function(guid) {
         code: 0, 
         info: {
             counts: protein_counts,
-            proteins: protein_id
+            backgrounds: xrefs,
+            summary: summary_info
         }
     }) 
     |> writeLines(con = buffer("text"))
