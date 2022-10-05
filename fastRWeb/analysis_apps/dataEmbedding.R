435508a9fb98d21dc4527815a928e7337d6f80b9
xieguigang <xie.guigang@gcmodeller.org>
2022-10-06 02:12:35 +0800

    Update dataEmbedding.R

--- fastRWeb/analysis_apps/dataEmbedding.R
+++ fastRWeb/analysis_apps/dataEmbedding.R
@@ -1,5 +1,9 @@
 require(JSON);
 
+imports ["dataset", "umap", "t-SNE"] from "MLkit";
+
+#' Do data embedding with different algorithm
+#' 
 const run = function(file, ssid, dims = 3, algorithm = ["PCA", "t-SNE", "UMAP"]) {
     const session_file = `${getOption("system_tempdir")}/${session_id}/dataEmbedding.dat`;
     const matrix = read.csv(file, row.names = 1, check.names = FALSE);
@@ -21,7 +25,21 @@ const run = function(file, ssid, dims = 3, algorithm = ["PCA", "t-SNE", "UMAP"])
 }
 
 const run_umap = function(matrix, dims = 3) {
-
+    const manifold = matrix
+    |> umap(
+        dimension            = dims, 
+        numberOfNeighbors    = 15,
+        localConnectivity    = 1,
+        KnnIter              = 64,
+        bandwidth            = 1
+    )
+    ;
+    
+    as.data.frame(manifold$umap, 
+        labels = manifold$labels, 
+        dimension = `dim${1:dims}`
+    )
+    ;
 }
 
 const run_pca = function(matrix, dims = 3) {
@@ -29,5 +47,12 @@ const run_pca = function(matrix, dims = 3) {
 }
 
 const run_tsne = function(matrix, dims = 3) {
-
+    const tSNE = t.SNE()
+	|> data(matrix)
+	|> solve(iterations = 200)	
+	;
+	
+	tSNE 
+	|> as.data.frame(row.names = tags) 
+	;
 }
