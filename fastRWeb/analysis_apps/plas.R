771b3ec51331e2152a8c874d422bff8bd37a79a5
xieguigang <xie.guigang@gcmodeller.org>
2022-10-06 23:50:58 +0800

    decode json

--- fastRWeb/analysis_apps/plas.R
+++ fastRWeb/analysis_apps/plas.R
@@ -4,6 +4,9 @@ require(GCModeller);
 const run = function(odes, constants, session_id, final_time = 5, resolution = 10000) {
     const session_file as string = `${getOption("system_tempdir")}/${session_id}/plas.dat`;
 
+    odes = json_decode(odes);
+    constants = json_decode(constants);
+
     print("view of the dynamics system inputs:");
     print("odes");
     str(odes);
