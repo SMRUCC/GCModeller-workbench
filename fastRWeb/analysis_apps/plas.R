eec6897d3fe817d0022e425f845f83d90364465a
xieguigang <xie.guigang@gcmodeller.org>
2022-10-07 00:06:32 +0800

    Update plas.R

--- fastRWeb/analysis_apps/plas.R
+++ fastRWeb/analysis_apps/plas.R
@@ -1,8 +1,10 @@
 require(JSON);
 require(GCModeller);
 
+imports "S.system" from "simulators";
+
 const run = function(odes, constants, session_id, final_time = 5, resolution = 10000) {
-    const session_file as string = `${getOption("system_tempdir")}/${session_id}/plas.dat`;
+    const session_file as string = `${getOption("system_tempdir")}/${session_id}/plas.csv`;
 
     odes = json_decode(odes);
     constants = json_decode(constants);
@@ -17,5 +19,39 @@ const run = function(odes, constants, session_id, final_time = 5, resolution = 1
     print("resolution");
     print(resolution);
 
-    stop("123");
+    # set y0 of the ODEs system
+    const symbol_names = sapply(odes, x -> first(strsplit(x$target, "=", fixed = TRUE)));
+    const symbols = lapply(odes, x -> as.numeric(x$value), names = symbol_names);
+    const consts = lapply(constants, x -> as.numeric(x$value), names = x -> x$target);
+    const ssystem = lapply(odes, function(t) {
+        const split = unlist(strsplit(t$target, "=", fixed = TRUE));
+        const expr = split[2];
+
+        expr;
+    });
+
+    names(ssystem) = symbol_names;
+    consts$is.const = TRUE;
+
+    print("get odes symbols y0:");
+    str(symbols);
+    print("symbol expressions:");
+    str(ssystem);
+
+    using data.driver as snapshot(session_file, symbols = names(symbols)) {
+        data.driver
+        |> kernel(S.script(session_id))
+        |> environment(symbols = consts)
+        |> environment(symbols = symbols)
+        |> s.system(ssystem = ssystem)
+        |> run(ticks = final_time, resolution = 1/as.numeric(resolution))
+        ;
+    }
+
+    json_encode({
+        code: 0,
+        info: `/@temp/${session_id}/plas.csv`
+    })
+    |> writeLines(con = buffer("text"))
+    ;
 }
