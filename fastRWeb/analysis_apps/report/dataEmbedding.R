eded4412a345ded103581629f6ba71e208cc03b1
xieguigang <xie.guigang@gcmodeller.org>
2022-10-06 14:47:11 +0800

    create zip file for download result

--- fastRWeb/analysis_apps/report/dataEmbedding.R
+++ fastRWeb/analysis_apps/report/dataEmbedding.R
@@ -5,7 +5,16 @@ const run = function(ssid) {
     const saveimage = `${getOption("system_tempdir")}/${ssid}/dataEmbedding.png`;
     const csvfile_export = `${getOption("system_tempdir")}/${ssid}/dataEmbedding.csv`;
     const data = readRDS(session_file);
+    const zipfile = `${getOption("system_tempdir")}/${ssid}/download.zip`;
 
     write.csv(data, file = csvfile_export, row.names = TRUE);
-    
+
+    zip(zipfile, [session_file, saveimage, csvfile_export]);
+
+    json_encode({
+        code: 0,
+        info: `/@temp/${ssid}/download.zip`
+    })
+    |> writeLines(con = buffer(text))
+    ;
 }
