93d76c02038c1b40d93f108858059028abf5f377
xieguigang <xie.guigang@gcmodeller.org>
2022-10-06 23:45:22 +0800

    view parameters input in R script

--- fastRWeb/analysis_apps/plas.R
+++ fastRWeb/analysis_apps/plas.R
@@ -1,6 +1,18 @@
 require(JSON);
 require(GCModeller);
 
-const run = function(ssid) {
-    const session_file as string = `${getOption("system_tempdir")}/${ssid}/plas.dat`;
+const run = function(odes, constants, session_id, final_time = 5, resolution = 10000) {
+    const session_file as string = `${getOption("system_tempdir")}/${session_id}/plas.dat`;
+
+    print("view of the dynamics system inputs:");
+    print("odes");
+    str(odes);
+    print("constants");
+    str(constants);
+    print("t");
+    print(final_time);
+    print("resolution");
+    print(resolution);
+
+    stop("123");
 }
