b905ee760dc4ad0b14b2bfe92e4334639a0f6cf4
xieguigang <xie.guigang@gcmodeller.org>
2022-10-09 23:17:12 +0800

    Create Project.vb

---
+++ src/OmicsProject/Project.vb
@@ -0,0 +1,11 @@
+﻿Imports SMRUCC.genomics.Analysis.HTS.DataFrame
+Imports SMRUCC.genomics.GCModeller.Workbench.ExperimentDesigner
+
+Public Class Project
+
+    Public Property samples As SampleInfo()
+    Public Property raw As Matrix
+    Public Property normalized As Matrix
+    Public Property transformed As Matrix
+
+End Class
