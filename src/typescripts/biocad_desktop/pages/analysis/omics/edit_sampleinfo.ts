36197d892dda06b870d030a1c93400f4e9380088
xieguigang <xie.guigang@gcmodeller.org>
2022-10-16 15:43:45 +0800

    required a sampleinfo editor

---
+++ src/typescripts/biocad_desktop/pages/analysis/omics/edit_sampleinfo.ts
@@ -0,0 +1,14 @@
+namespace pages.analysis_project {
+
+    export class edit_sampleinfo extends Bootstrap {
+
+        public get appName(): string {
+            return "edit_sampleinfo";
+        };
+
+        protected init(): void {
+            // throw new Error("Method not implemented.");
+        }
+
+    }
+}
