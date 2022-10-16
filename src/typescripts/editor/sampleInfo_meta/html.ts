be85bb936a914e33fac5192c68e81dfe8f25ea57
xieguigang <xie.guigang@gcmodeller.org>
2022-10-16 16:10:14 +0800

    split source function and make bugs fixed for symbol reference error

--- src/typescripts/editor/sampleInfo_meta/html.ts
+++ src/typescripts/editor/sampleInfo_meta/html.ts
@@ -3,7 +3,7 @@ namespace sampleinfo_editor {
     /**
      * 预设的颜色
     */
-    const colors: string[] = [
+    export const colors: string[] = [
         "#E41A1C",
         "#377EB8",
         "#4DAF4A",
@@ -14,7 +14,7 @@ namespace sampleinfo_editor {
         "#F781BF",
         "#999999"
     ];
-    const shapes: {} = {
+    export const shapes: {} = {
         shape21: "●",
         shape22: "■",
         shape23: "◆",
@@ -27,121 +27,10 @@ namespace sampleinfo_editor {
         shape6: "▽"
     };
 
-    let colorPickers: {} = {};
-    let instance: metaEditor;
+    export let colorPickers: {} = {};
+    export let instance: metaEditor;
 
-    export class metaEditor {
-
-        public constructor(public sampleMeta: IsampleMeta[], div: string) {
-            let table: IHTMLElement = $ts("<table>", { class: "table" });
-            let header = $ts("<thead>").appendElement(
-                $ts("<tr>")
-                    .appendElement($ts("<th>").display("组别名称"))
-                    .appendElement($ts("<th>").display("颜色"))
-                    .appendElement($ts("<th>").display("形状"))
-            );
-            let body = $ts("<tbody>");
-            let row: IHTMLElement;
-            let pickers: string[] = [];
-            let labels: {} = {};
-            let defaultColors: {} = {};
-            let metadata: {} = {};
-
-            for (let meta of sampleMeta) {
-                if (Strings.Empty(meta.color, true)) {
-                    meta.color = "ee3333";
-                }
-                if (Strings.Empty(`${meta.shape}`, true)) {
-                    meta.shape = 0;
-                }
-
-                let pickerId: string = `color-${meta.sampleInfo.replace(/\./g, "-")}`;
-
-                row = $ts("<tr>");
-                row.appendElement($ts("<td>").display(meta.sampleInfo));
-                row.appendElement($ts("<td>", { id: pickerId }));
-                // row.appendElement($ts("<td>").display(colorSetter(meta.sampleInfo, meta.color, <any>((label, value) => this.colorSetter(label, value)))));
-                row.appendElement($ts("<td>").display(shapeSetter(meta.sampleInfo, <any>meta.shape, <any>((label, value) => this.shapeSetter(label, value)))));
-
-                pickers.push(pickerId);
-                labels[pickerId] = meta.sampleInfo;
-                defaultColors[pickerId] = Strings.Empty(meta.color, true) ? "#ee3333" : ("#" + meta.color);
-                metadata[pickerId] = meta;
-
-                body.appendElement(row);
-            }
-
-            table.appendElement(header).appendElement(body);
-
-            $ts(div)
-                .clear()
-                .appendElement(table);
-
-            let color: string;
-            let colorPicker: uikit.colorPicker.colorPickerUI;
-
-            for (let id of pickers) {
-                let currentLabel = metaEditor.holdLabel(labels[id]);
-
-                colorPicker = uikit.colorPicker.fast("#" + id, colors, this.setColor(labels[id]), defaultColors[id], function () {
-                    currentSampleinfo = currentLabel();
-                    setMoreColors();
-                });
-                color = colorPicker.color;
-                color = color.replace(/[#]+/g, "");
-
-                colorPickers[labels[id]] = colorPicker;
-
-                (<IsampleMeta>metadata[id]).color = color;
-            }
-
-            instance = this;
-        }
-
-        private static jqueryColorPickerUI(id: string, _default: string) {
-            return $ts("<div>", {
-                style: "width:128px;"
-            }).display($ts("<input>", {
-                style: "width:100px;",
-                id: id,
-                name: id,
-                class: ["colorPicker", "evo-cp0"],
-                placeholder: _default,
-                value: _default
-            }))
-        }
-
-        private static holdLabel(label: string) {
-            return function (): string {
-                return label;
-            }
-        }
-
-        public setColor(label: string) {
-            let vm = this;
-
-            return function (color: string) {
-                vm.colorSetter(label, color.substr(1));
-            }
-        }
-
-        shapeSetter(label: string, value: string) {
-            $from(this.sampleMeta).Where(a => a.sampleInfo == label).First.shape = <any>value;
-        }
-
-        colorSetter(label: string, value: string) {
-            let sample = $from(this.sampleMeta).Where(a => a.sampleInfo == label).First;
-            let colorModel = new TypeScript.ColorManager.w3color("#" + value);
-            let a = new TypeScript.ColorManager.w3color(`rgb(${Math.round(colorModel.red / 2)}, ${Math.round(colorModel.green / 2)}, ${Math.round(colorModel.blue / 2)})`);
-            let b = new TypeScript.ColorManager.w3color(`rgb(${255 - Math.round(colorModel.red / 2)}, ${255 - Math.round(colorModel.green / 2)}, ${255 - Math.round(colorModel.blue / 2)})`);
-
-            sample.color = value;
-            sample.color1 = a.toHexString().substr(1);
-            sample.color2 = b.toHexString().substr(1);
-        }
-    }
-
-    function shapeSetter(label: string, _default: string, setValue: Delegate.Sub) {
+    export function shapeSetter(label: string, _default: string, setValue: Delegate.Sub) {
         let opt = $ts("<select>", {
             onchange: function () {
                 let strVal: string = (<HTMLSelectElement><any>opt).value;
@@ -180,7 +69,7 @@ namespace sampleinfo_editor {
     let currentLabel: string;
     let currentMoreOpt: HTMLElement;
 
-    function colorSetter(label: string, _default: string, setValue: Delegate.Sub) {
+    export function colorSetter(label: string, _default: string, setValue: Delegate.Sub) {
         // 选择更多颜色
         let setMore = function () {
             current = <any>opt;
@@ -250,7 +139,7 @@ namespace sampleinfo_editor {
         }
     }
 
-    let currentSampleinfo: string;
+    export let currentSampleinfo: string = "n/a";
 
     export function setColorValue(color: TypeScript.ColorManager.w3color) {
         let colorStr: string = color.toHexString();
@@ -261,13 +150,13 @@ namespace sampleinfo_editor {
         //currentMoreOpt.style.backgroundColor = colorStr;
         //currentMoreOpt.innerHTML = colorStr + " [重新选择颜色]";
 
-        (<uikit.colorPicker.colorPickerUI>colorPickers[currentSampleinfo]).setDisplayColor(colorStr);
+        (<colorPicker.colorPickerUI>colorPickers[currentSampleinfo]).setDisplayColor(colorStr);
         instance.setColor(currentSampleinfo)(colorStr);
 
         console.log(colorStr);
     }
 
-    function setMoreColors() {
+    export function setMoreColors() {
         console.log("设置更多的颜色");
 
         $ts("#colorBox").show()
---
+++ src/typescripts/editor/sampleInfo_meta/metaEditor.ts
@@ -0,0 +1,113 @@
+namespace sampleinfo_editor {
+
+    export class metaEditor {
+
+        public constructor(public sampleMeta: IsampleMeta[], div: string) {
+            let table: IHTMLElement = $ts("<table>", { class: "table" });
+            let header = $ts("<thead>").appendElement(
+                $ts("<tr>")
+                    .appendElement($ts("<th>").display("组别名称"))
+                    .appendElement($ts("<th>").display("颜色"))
+                    .appendElement($ts("<th>").display("形状"))
+            );
+            let body = $ts("<tbody>");
+            let row: IHTMLElement;
+            let pickers: string[] = [];
+            let labels: {} = {};
+            let defaultColors: {} = {};
+            let metadata: {} = {};
+
+            for (let meta of sampleMeta) {
+                if (Strings.Empty(meta.color, true)) {
+                    meta.color = "ee3333";
+                }
+                if (Strings.Empty(`${meta.shape}`, true)) {
+                    meta.shape = 0;
+                }
+
+                let pickerId: string = `color-${meta.sampleInfo.replace(/\./g, "-")}`;
+
+                row = $ts("<tr>");
+                row.appendElement($ts("<td>").display(meta.sampleInfo));
+                row.appendElement($ts("<td>", { id: pickerId }));
+                // row.appendElement($ts("<td>").display(colorSetter(meta.sampleInfo, meta.color, <any>((label, value) => this.colorSetter(label, value)))));
+                row.appendElement($ts("<td>").display(shapeSetter(meta.sampleInfo, <any>meta.shape, <any>((label, value) => this.shapeSetter(label, value)))));
+
+                pickers.push(pickerId);
+                labels[pickerId] = meta.sampleInfo;
+                defaultColors[pickerId] = Strings.Empty(meta.color, true) ? "#ee3333" : ("#" + meta.color);
+                metadata[pickerId] = meta;
+
+                body.appendElement(row);
+            }
+
+            table.appendElement(header).appendElement(body);
+
+            $ts(div)
+                .clear()
+                .appendElement(table);
+
+            let color: string;
+            let colorPicker: colorPicker.colorPickerUI;
+
+            for (let id of pickers) {
+                let currentLabel = metaEditor.holdLabel(labels[id]);
+
+                colorPicker = colorPicker.fast("#" + id, colors, this.setColor(labels[id]), defaultColors[id], function () {
+                    currentSampleinfo = currentLabel();
+                    setMoreColors();
+                });
+                color = colorPicker.color;
+                color = color.replace(/[#]+/g, "");
+
+                colorPickers[labels[id]] = colorPicker;
+
+                (<IsampleMeta>metadata[id]).color = color;
+            }
+
+            instance = this;
+        }
+
+        private static jqueryColorPickerUI(id: string, _default: string) {
+            return $ts("<div>", {
+                style: "width:128px;"
+            }).display($ts("<input>", {
+                style: "width:100px;",
+                id: id,
+                name: id,
+                class: ["colorPicker", "evo-cp0"],
+                placeholder: _default,
+                value: _default
+            }))
+        }
+
+        private static holdLabel(label: string) {
+            return function (): string {
+                return label;
+            }
+        }
+
+        public setColor(label: string) {
+            let vm = this;
+
+            return function (color: string) {
+                vm.colorSetter(label, color.substr(1));
+            }
+        }
+
+        shapeSetter(label: string, value: string) {
+            $from(this.sampleMeta).Where(a => a.sampleInfo == label).First.shape = <any>value;
+        }
+
+        colorSetter(label: string, value: string) {
+            let sample = $from(this.sampleMeta).Where(a => a.sampleInfo == label).First;
+            let colorModel = new TypeScript.ColorManager.w3color("#" + value);
+            let a = new TypeScript.ColorManager.w3color(`rgb(${Math.round(colorModel.red / 2)}, ${Math.round(colorModel.green / 2)}, ${Math.round(colorModel.blue / 2)})`);
+            let b = new TypeScript.ColorManager.w3color(`rgb(${255 - Math.round(colorModel.red / 2)}, ${255 - Math.round(colorModel.green / 2)}, ${255 - Math.round(colorModel.blue / 2)})`);
+
+            sample.color = value;
+            sample.color1 = a.toHexString().substr(1);
+            sample.color2 = b.toHexString().substr(1);
+        }
+    }
+}
