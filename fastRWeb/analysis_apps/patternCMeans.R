2e0ec2b76d250d42b965a52b848ce777681d85d0
xieguigang <xie.guigang@gcmodeller.org>
2022-10-16 23:33:06 +0800

    Update patternCMeans.R

--- fastRWeb/analysis_apps/patternCMeans.R
+++ fastRWeb/analysis_apps/patternCMeans.R
@@ -6,6 +6,19 @@ imports "geneExpression" from "phenotype_kit";
 const run = function(file, ssid, z_score = TRUE, algorithm = ["cmeans", "kmeans"], layout = [3,3]) {
     const expr0 = matrixFileReader(file, z_score);
     const patterns = expression.cmeans_pattern(expr0, dim = layout);
+    const session_file as string = `${getOption("system_tempdir")}/${ssid}/patterns_data.dat`;
+    const session_mat as string = `${getOption("system_tempdir")}/${ssid}/patterns.csv`;
+    const data = cmeans_matrix(patterns);
+
+    write.csv(data, file = session_mat);
+    savePattern(patterns, file = session_file);
+
+    const peeks = head(read.csv(session_mat, row.names = 1, check.names = FALSE), 10);
+
+    print("preview of the result:");
+    print(peeks);
+
+    write.csv(peeks, file = buffer("dataframe"), tsv = TRUE);
 }
 
 const matrixFileReader = function(file, z_score = TRUE) {
