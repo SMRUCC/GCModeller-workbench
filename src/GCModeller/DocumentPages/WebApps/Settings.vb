9b9074c9ab421ad34f82fb885fe7fc371f349676
xieguigang <xie.guigang@gcmodeller.org>
2022-10-22 01:26:43 +0800

    config for webview2 hosting

--- src/GCModeller/DocumentPages/WebApps/Settings.vb
+++ src/GCModeller/DocumentPages/WebApps/Settings.vb
@@ -5,5 +5,9 @@
         Public Sub New()
             MyBase.New("/settings.vbhtml")
         End Sub
+
+        Public Function GetSettings() As Global.GCModeller_win32Desktop.Settings.File
+            Return Global.GCModeller_win32Desktop.Settings.Session.SettingsFile
+        End Function
     End Class
 End Namespace
