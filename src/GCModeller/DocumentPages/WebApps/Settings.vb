b4a108a107cc0a9de86743d06de37c0b49251bbd
xieguigang <xie.guigang@gcmodeller.org>
2022-10-22 10:03:45 +0800

    how to do data configurations

--- src/GCModeller/DocumentPages/WebApps/Settings.vb
+++ src/GCModeller/DocumentPages/WebApps/Settings.vb
@@ -1,4 +1,8 @@
-﻿Namespace WebApps
+﻿Imports Microsoft.VisualBasic.MIME.application.json
+Imports Microsoft.VisualBasic.MIME.application.json.Javascript
+Imports Microsoft.VisualBasic.My.JavaScript
+
+Namespace WebApps
 
     Public Class Settings : Inherits WebApp
 
@@ -8,6 +12,11 @@
 
         Public Function GetSettings() As Global.GCModeller_win32Desktop.Settings.File
             Return Global.GCModeller_win32Desktop.Settings.Session.SettingsFile
+        End Function
+
+        Public Function SaveSettings(config As String)
+            Dim configs As JavaScriptObject = DirectCast(config.ParseJson, JsonObject)
+
         End Function
     End Class
 End Namespace
