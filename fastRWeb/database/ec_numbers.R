b0421e1302f4d7ab74c9cfa5f9dec3eb28804161
xieguigang <xie.guigang@gcmodeller.org>
2022-10-24 22:16:27 +0800

    wrap make blastdb

--- fastRWeb/database/ec_numbers.R
+++ fastRWeb/database/ec_numbers.R
@@ -4,11 +4,13 @@ require(Rstudio);
 imports "UniProt" from "annotationKit";
 imports "uniprot" from "seqtoolkit";
 imports "bioseq.fasta" from "seqtoolkit";
+imports "blast+" from "seqtoolkit";
 
 const run = function(uniprot) {
 	const ecNumbersDbfile as string = `${RStudio::repository_root()}/EC_numbers.db`;
 	const ecNumbersFasta as string = `${dirname(ecNumbersDbfile)}/EC_numbers.fasta`;
-	const ncbi_blast = RStudio::ncbi_blast_dir();
+	
+	options(ncbi_blast = RStudio::ncbi_blast_dir());
 
 	using pack as UniProt::ECnumber_pack(ecNumbersDbfile, create_new = TRUE) {
 		pack |> add_ecNumbers(			
