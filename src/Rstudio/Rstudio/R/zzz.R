363be9fe5224ddd6dabc23f51a2f8cb98f10fa77
xieguigang <xie.guigang@gcmodeller.org>
2022-10-24 23:19:42 +0800

    config workbench runtime

--- src/Rstudio/Rstudio/R/zzz.R
+++ src/Rstudio/Rstudio/R/zzz.R
@@ -7,4 +7,7 @@ imports "RStudio" from "GCModellerDesktop";
 const .onLoad = function() {
     # load config repository automatically
     RStudio::config(getOption("gcmodeller.config"));
+
+    print("Load workbench desktop runtime!");
+    print(`RStudio::config(${getOption("gcmodeller.config")})`);
 }
--- src/Rstudio/Rstudio/man/GCModellerDesktop/RStudio/RStudio.md
+++ src/Rstudio/Rstudio/man/GCModellerDesktop/RStudio/RStudio.md
@@ -3,3 +3,5 @@
 
 
 + [repository_root](RStudio/repository_root.1) 
++ [ncbi_blast_dir](RStudio/ncbi_blast_dir.1) 
++ [config](RStudio/config.1) 
