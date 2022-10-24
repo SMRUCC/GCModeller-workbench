0365ed4ac25d98a3a23d080e15298740a5a5d507
xieguigang <xie.guigang@gcmodeller.org>
2022-10-24 21:54:44 +0800

    config Rstudio packages

--- src/Rstudio/Rstudio/R/zzz.R
+++ src/Rstudio/Rstudio/R/zzz.R
@@ -2,6 +2,7 @@ require(JSON);
 require(GCModeller);
 
 imports "geneExpression" from "phenotype_kit";
+imports "RStudio" from "";
 
 const .onLoad = function() {
 
---
+++ src/Rstudio/Rstudio/src/RStudio.vb
@@ -0,0 +1,6 @@
+Imports Microsoft.VisualBasic.Scripting.MetaData
+
+<Package("RStudio")>
+Public Module RStudio
+
+End Module
