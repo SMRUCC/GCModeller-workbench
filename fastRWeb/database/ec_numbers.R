f04c70ed509996243db78fc88a818b68207c9833
xieguigang <xie.guigang@gcmodeller.org>
2022-10-24 21:42:46 +0800

    config projects for build r packages api

--- fastRWeb/database/ec_numbers.R
+++ fastRWeb/database/ec_numbers.R
@@ -1,27 +1,23 @@
-imports "package_utils" from "devkit";
-
-package_utils::attach("E:\GCModeller\src\workbench\pkg");
-
 require(GCModeller);
 
 imports "UniProt" from "annotationKit";
 imports "uniprot" from "seqtoolkit";
 imports "bioseq.fasta" from "seqtoolkit";
 
-using pack as UniProt::ECnumber_pack("K:\Downloads\EC_numbers.db", create_new = TRUE) {
+const run = function(uniprot) {
+	const ecNumbersDbfile as string = `/EC_numbers.db`;
+	const ecNumbersFasta as string = `${dirname(ecNumbersDbfile)}/EC_numbers.fasta`;
 
-	pack |> add_ecNumbers(
-		"K:\Downloads\uniprot-compressed_true_download_true_format_xml_query__28_28taxonom-2022.10.02-09.21.31.14.xml"
-		|> open.uniprot()
-	);
+	using pack as UniProt::ECnumber_pack(ecNumbersDbfile, create_new = TRUE) {
+		pack |> add_ecNumbers(			
+			open.uniprot(uniprot)
+		);
+	}
 
-}
-
-using pack as UniProt::ECnumber_pack("K:\Downloads\EC_numbers.db", create_new = FALSE) {
-
-	pack 
-	|> extract_fasta()
-	|> write.fasta(file = "K:\Downloads\EC_numbers.fasta")
-	;
-	
+	using pack as UniProt::ECnumber_pack(ecNumbersDbfile, create_new = FALSE) {
+		pack 
+		|> extract_fasta()
+		|> write.fasta(file = ecNumbersFasta)
+		;
+	}
 }
