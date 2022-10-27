552effdd91735306b6285d0843d01dcfba2bfca4
xieguigang <xie.guigang@gcmodeller.org>
2022-10-27 22:27:23 +0800

    create project

---
+++ src/Rstudio/Rstudio/src/Modeller.vb
@@ -0,0 +1,26 @@
+﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.genomics.Assembly.NCBI.GenBank
+Imports SMRUCC.genomics.Model
+Imports SMRUCC.genomics.Model.BioSystems
+Imports SMRUCC.Rsharp.Runtime
+Imports SMRUCC.Rsharp.Runtime.Components
+Imports SMRUCC.Rsharp.Runtime.Interop
+
+<Package("Modeller")>
+Module Modeller
+
+    <ExportAPI("loadProject")>
+    <RApiReturn(GetType(Project))>
+    Public Function createProject(assembly As Object, Optional env As Environment = Nothing) As Object
+        If assembly Is Nothing Then
+            Return Nothing
+        End If
+
+        If TypeOf assembly Is GBFF.File Then
+            Return BioSystems.Utils.FromGenBank(genbank:=assembly)
+        Else
+            Return Message.InCompatibleType(GetType(GBFF.File), assembly.GetType, env)
+        End If
+    End Function
+End Module
