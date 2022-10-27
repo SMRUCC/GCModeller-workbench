4d80d8363de93ce8190a60b87a45d62338503392
xieguigang <xie.guigang@gcmodeller.org>
2022-10-27 23:11:33 +0800

    create project

--- fastRWeb/modeller/create_project.R
+++ fastRWeb/modeller/create_project.R
@@ -1,6 +1,20 @@
 require(JSON);
 require(GCModeller);
 
-const run = function(gbff, savefile) {
+imports "GenBank" from "seqtoolkit";
+imports "Modeller" from "GCModellerDesktop";
 
+const run = function(gbff, savefile) {
+    gbff
+    |> read.genbank()
+    |> loadProject()
+    |> writeProject(file = savefile)
+    ;
+
+    json_encode({
+        code: 0,
+        info:""
+    })
+    |> writeLines(con = buffer("text"))
+    ;
 }
