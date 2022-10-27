734c171bdcac080e145807d5820229ea8fd085a7
xieguigang <xie.guigang@gcmodeller.org>
2022-10-27 22:30:31 +0800

    Update Modeller.vb

--- src/Rstudio/Rstudio/src/Modeller.vb
+++ src/Rstudio/Rstudio/src/Modeller.vb
@@ -1,4 +1,5 @@
-﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
+﻿Imports System.IO
+Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Scripting.MetaData
 Imports SMRUCC.genomics.Assembly.NCBI.GenBank
 Imports SMRUCC.genomics.Model
@@ -22,5 +23,21 @@ Module Modeller
         Else
             Return Message.InCompatibleType(GetType(GBFF.File), assembly.GetType, env)
         End If
+    End Function
+
+    <ExportAPI("writeProject")>
+    <RApiReturn(GetType(Boolean))>
+    Public Function writeProject(proj As Project, file As Object, Optional env As Environment = Nothing) As Object
+        Dim buffer = SMRUCC.Rsharp.GetFileStream(file, FileAccess.ReadWrite, env)
+
+        If buffer Like GetType(Message) Then
+            Return buffer.TryCast(Of Message)
+        End If
+
+        Using writer As New ProjectWriter(buffer.TryCast(Of Stream))
+            Call writer.WriteProject(proj)
+        End Using
+
+        Return True
     End Function
 End Module
