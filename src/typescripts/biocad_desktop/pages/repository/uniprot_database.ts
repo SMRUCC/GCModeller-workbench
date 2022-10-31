e015774c26859f0e1c83c663a735f2a5d4d060f1
xieguigang <xie.guigang@gcmodeller.org>
2022-10-31 22:02:31 +0800

    install uniprot database

--- src/typescripts/biocad_desktop/pages/repository/uniprot_database.ts
+++ src/typescripts/biocad_desktop/pages/repository/uniprot_database.ts
@@ -11,11 +11,33 @@ namespace pages.repository {
         }
 
         public button_open_uniprot_onclick() {
-
+            desktop.winforms.openfileDialog("#uniprot-file", apps.uniprot_assembly);
         }
 
         public run_onclick() {
+            desktop.loading("Build blast database...");
 
+            const textbox = $ts("#uniprot-file").CType<HTMLInputElement>();
+            const path: string = textbox.value;
+            const json = {
+                uniprot: path
+            };
+            const title: string = "Build enzyme blast+ database";
+
+            if (Strings.Empty(path, true)) {
+                desktop.showToastMessage("No uniprot assembly file was provided!", title, "danger");
+                return;
+            }
+
+            apps.gcmodeller
+                .createTask(`Imports Enzyme Database`, $ts.url("@web_invoke_imports"), JSON.stringify(json))
+                .then(async function (msg) {
+                    const task_id: string = await msg.data;
+
+                    desktop.showTaskAlert();
+                    desktop.showToastMessage(`Pending a new task: ${task_id}`, title, "info");
+                })
+                ;
         }
     }
 }
