03f92c1b489901dc779c8d22a7b81453c76d7adc
xieguigang <xie.guigang@gcmodeller.org>
2022-11-03 22:37:15 +0800

    dumpping demo data

--- src/Rstudio/test/inspect_ptf.R
+++ src/Rstudio/test/inspect_ptf.R
@@ -1,4 +1,5 @@
 require(HDS);
+require(JSON);
 
 db = HDS::openStream("E:\GCModeller\src\workbench\win32_desktop\demo\ncbi_dataset\GCF_000005845\eco.bioproj");
 
@@ -10,8 +11,15 @@ writeLines(tree, con = `${@dir}/eco_project.txt`);
 
 
 db = HDS::openStream("E:\etc\repository\EC_numbers.db");
+
+list = bdecode(db |> HDS::getText("/subcellularLocation.txt"));
+
+writeLines(json_encode(list), con = `${@dir}/subcellularLocation.json`);
+
 tree = HDS::tree(db);
 
-print(tree);
+# print(tree);
 
 writeLines(tree, con = `${@dir}/ec_numbers.txt`);
+writeLines(db |> HDS::getText("/subcellularLocation.txt"), con = `${@dir}/subcellularLocation.txt`);
+
