ee8cb62ea9183608369519c7c59bf08865c6f833
xieguigang <xie.guigang@gcmodeller.org>
2022-11-07 00:29:40 +0800

    Update bioproject.ts

--- src/typescripts/biocad_desktop/pages/modeller/bioproject.ts
+++ src/typescripts/biocad_desktop/pages/modeller/bioproject.ts
@@ -16,6 +16,47 @@ namespace pages.modeller {
             $ts("#path").display(this.path);
         }
 
+        private static showPiePlot(summary: any) {
+            const total = summary.total;
+            const enzyme_numbers = summary.number_enzymes;
+            const no_annotation = (total - enzyme_numbers) < 0 ? 0 : (total - enzyme_numbers);
+
+            // show summary pie
+            new js_plot.piePlot("Protein Annotations", "Metabolic Annotation", "summary_pie").plot(
+                "protein numbers", [
+                { name: "enzymes", value: enzyme_numbers },
+                { name: "no annotation", value: no_annotation }
+            ]);
+
+            const enzyme_annotations: {} = summary.enzyme_class;
+            const count1 = $from(Object.keys(enzyme_annotations))
+                .Select(function (label) {
+                    return {
+                        name: label,
+                        value: enzyme_annotations[label]
+                    }
+                })
+                .ToArray();
+
+            new js_plot.piePlot("Metabolic Annotation", "Enzyme Class Counts", "summary_enzyme_pie")
+                .plot("protein numbers", count1)
+                ;
+
+            const subcellular_locations: {} = summary.subcellular_locations;
+            const count2 = $from(Object.keys(subcellular_locations))
+                .Select(function (label) {
+                    return {
+                        name: label,
+                        value: subcellular_locations[label]
+                    }
+                })
+                .ToArray();
+
+            new js_plot.piePlot("Subcellular Locations Annotation", "Metabolic Locations", "summary_location_pie")
+                .plot("protein numbers", count2)
+                ;
+        }
+
         private loadSummary(path: string) {
             const url: string = `@web_invoke_inspect_model?proj=${encodeURIComponent(path)}`;
 
@@ -24,46 +65,8 @@ namespace pages.modeller {
                     desktop.showToastMessage("Load project file error!", "Project Reader", "danger");
                 } else {
                     const summary: any = result.info;
-                    const total = summary.total;
-                    const enzyme_numbers = summary.number_enzymes;
-                    const no_annotation = (total - enzyme_numbers) < 0 ? 0 : (total - enzyme_numbers);
-
                     console.log(summary);
-
-                    // show summary pie
-                    new js_plot.piePlot("Protein Annotations", "Metabolic Annotation", "summary_pie").plot(
-                        "protein numbers", [
-                        { name: "enzymes", value: enzyme_numbers },
-                        { name: "no annotation", value: no_annotation }
-                    ]);
-
-                    const enzyme_annotations: {} = summary.enzyme_class;
-                    const count1 = $from(Object.keys(enzyme_annotations))
-                        .Select(function (label) {
-                            return {
-                                name: label,
-                                value: enzyme_annotations[label]
-                            }
-                        })
-                        .ToArray();
-
-                    new js_plot.piePlot("Metabolic Annotation", "Enzyme Class Counts", "summary_enzyme_pie")
-                        .plot("protein numbers", count1)
-                        ;
-
-                    const subcellular_locations: {} = summary.subcellular_locations;
-                    const count2 = $from(Object.keys(subcellular_locations))
-                        .Select(function (label) {
-                            return {
-                                name: label,
-                                value: subcellular_locations[label]
-                            }
-                        })
-                        .ToArray();
-
-                    new js_plot.piePlot("Subcellular Locations Annotation", "Metabolic Locations", "summary_location_pie")
-                        .plot("protein numbers", count2)
-                        ;
+                    bioproject.showPiePlot(summary);
                 }
             });
         }
