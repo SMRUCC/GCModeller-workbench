250476e3cc4e4d20753e83f27ca2d6fe2ccfb60c
xieguigang <xie.guigang@gcmodeller.org>
2022-11-08 00:35:38 +0800

    add api for get blast data

--- src/GCModeller/DocumentPages/WebApps/Viewers/BlastpViewer.vb
+++ src/GCModeller/DocumentPages/WebApps/Viewers/BlastpViewer.vb
@@ -1,11 +1,26 @@
-﻿Imports SMRUCC.genomics.Interops.NCBI.Extensions.LocalBLAST.Application.BBH
+﻿Imports Microsoft.VisualBasic.Serialization.JSON
+Imports SMRUCC.genomics.Interops.NCBI.Extensions.LocalBLAST.Application.BBH
 
 Public Class BlastpViewer : Inherits WebApp
 
-    ReadOnly blastdata As BestHit()
+    ReadOnly blastdata As Dictionary(Of String, BestHit())
 
     Public Sub New(blastdata As BestHit())
         MyBase.New("/toolkit/viewer/blastp.vbhtml")
-        Me.blastdata = blastdata
+
+        Me.blastdata = blastdata _
+            .GroupBy(Function(a) a.QueryName) _
+            .ToDictionary(Function(prot) prot.Key,
+                          Function(hits)
+                              Return hits.ToArray
+                          End Function)
     End Sub
+
+    Public Function getProteinIDs() As String()
+        Return blastdata.Keys.ToArray
+    End Function
+
+    Public Function getBlastp(id As String) As String
+        Return blastdata(id).GetJson
+    End Function
 End Class
