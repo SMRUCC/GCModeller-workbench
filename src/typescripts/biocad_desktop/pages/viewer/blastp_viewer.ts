976db816d57b7ed588512d75a53e057ddffa9226
xieguigang <xie.guigang@gcmodeller.org>
2022-11-08 01:02:55 +0800

    removes un-necessary data fileds

--- src/typescripts/biocad_desktop/pages/viewer/blastp_viewer.ts
+++ src/typescripts/biocad_desktop/pages/viewer/blastp_viewer.ts
@@ -23,8 +23,21 @@ namespace pages.viewers {
         }
 
         private viewBlastp(id: string) {
-            const show = function (data) {
-                console.log(data);
+            const show = function (data: any[]) {
+                for (let i = 0; i < data.length; i++) {
+                    delete data[i].QueryName;
+                    delete data[i].HitName;
+                    delete data[i].hit_length;
+                    delete data[i].length_hit;
+                    delete data[i].length_hsp;
+                    delete data[i].length_query;
+                    delete data[i].query_length;
+
+                    data[i].evalue = data[i].evalue.toString();
+                }
+
+                $ts("#blast_output").clear();
+                $ts.appendTable(data, "#blast_output", null, { class: ["table", "table-sm"] });
             }
 
             apps.gcmodeller.getBlastp(id).then(async function (json) {
