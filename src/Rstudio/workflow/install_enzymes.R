9be0134478f168564806c9aec5264568b375ea5a
xieguigang <xie.guigang@gcmodeller.org>
2022-11-10 21:39:34 +0800

    add sabio-rk demo python script

--- src/Rstudio/workflow/install_enzymes.R
+++ src/Rstudio/workflow/install_enzymes.R
@@ -6,3 +6,15 @@ imports "enzymatic" from "vcellkit";
 setwd(@dir);
 
 const rhea = read.rhea("../data/rhea/rhea-reactions.txt");
+const sabio_rk = sabiork::new("./sabio-rk.db");
+
+for(reaction in rhea) {
+	ec_numbers = [reaction]::enzymes;
+	
+	for(num in ec_numbers) {
+		url = `https://sabiork.h-its.org/sabioRestWebServices/searchKineticLaws/sbml?q=ecnumber:${num}`;
+		sabiork::query(url, cache = sabio_rk);
+	}
+}
+
+close(sabio_rk);
