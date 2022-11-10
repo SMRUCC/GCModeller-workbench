aa1fe67d9e06074f79947a4ee14b2fee68641e98
xieguigang <xie.guigang@gcmodeller.org>
2022-11-11 00:00:49 +0800

    Update install_enzymes.R

--- src/Rstudio/workflow/install_enzymes.R
+++ src/Rstudio/workflow/install_enzymes.R
@@ -1,3 +1,7 @@
+imports "package_utils" from "devkit";
+
+package_utils::attach("E:\GCModeller\src\workbench\pkg");
+
 require(JSON);
 require(GCModeller);
 
@@ -10,7 +14,9 @@ const rhea = read.rhea("../data/rhea/rhea-reactions.txt");
 const sabio_rk = sabiork::new("./sabio-rk.db");
 
 for(reaction in rhea) {
-	ec_numbers = [reaction]::enzymes;
+	ec_numbers = [reaction]::enzyme;
+	
+	print(ec_numbers);
 	
 	for(num in ec_numbers) {
 		sabiork::query(num, cache = sabio_rk);
