2e9693448098853bf188db440737dc0b6d012afd
xieguigang <xie.guigang@gcmodeller.org>
2022-11-13 15:45:35 +0800

    Create test_kineticis.R

---
+++ src/Rstudio/workflow/test_kineticis.R
@@ -0,0 +1,18 @@
+imports "package_utils" from "devkit";
+
+package_utils::attach("E:\GCModeller\src\workbench\pkg");
+
+require(JSON);
+require(GCModeller);
+
+imports "enzymatic" from "vcellkit";
+imports "sabiork" from "vcellkit";
+
+const sabio_rk = sabiork::open("./sabio-rk.db");
+const kineticis = sabiork::get_kineticis(sabio_rk, "5.3.1.9");
+
+str(kineticis);
+
+for(law in kineticis) {
+	str(law);
+}
