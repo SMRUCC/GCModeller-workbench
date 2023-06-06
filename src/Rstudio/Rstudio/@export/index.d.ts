87ce2096ba20f3b3671c1635b088ace248cec024
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-06-06 22:08:50 +0800

    build documents

---
+++ src/Rstudio/Rstudio/@export/index.d.ts
@@ -0,0 +1,30 @@
+﻿// export R# source type define for javascript/typescript language
+//
+// package_source=Rstudio
+
+declare namespace Rstudio {
+   module _ {
+      /**
+      */
+      function onLoad(): object;
+   }
+   /**
+     * @param code default value Is ``0``.
+   */
+   function echo_successMsg(data: any, code?: object): object;
+   module fs {
+      /**
+      */
+      function ec_numbers_fasta(): object;
+      /**
+      */
+      function metabolic_db(): object;
+      /**
+      */
+      function subcellular_locations_fasta(): object;
+   }
+   /**
+     * @param z_score default value Is ``true``.
+   */
+   function matrixFileReader(file: any, z_score?: boolean): object;
+}
