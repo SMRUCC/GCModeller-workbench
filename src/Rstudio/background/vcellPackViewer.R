ff06cbd469ef18a424b557b21a93af3b18f0d23b
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-07-22 14:57:04 +0800

    create a web app background?

--- src/Rstudio/background/vcellPackViewer.R
+++ src/Rstudio/background/vcellPackViewer.R
@@ -1,8 +1,10 @@
 require(GCModeller);
 require(Rstudio);
 
-imports "Modeller" from "GCModellerDesktop";
+imports "Inspector" from "GCModellerDesktop";
 imports "rawXML" from "vcellkit";
 
 # the data backend R# script file for inspect the vcell data pack file
 const packfile as string = ?"--file" || stop("no result file was provided!");
+const view = Inspector::load(open.vcellPack(file = packfile, mode = "read"));
+
